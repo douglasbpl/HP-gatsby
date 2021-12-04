3bf452484cee36bf50b3f4c0ed8bf2312b5b97a7
douglasbpl <douglas.bpl94@yahoo.com.br>
2021-12-04 17:40:30 -0300

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create ({
-    baseURL: "http://hp-api.herokuapp.com/api/characters"
+    baseURL: "https://hp-api.herokuapp.com/api/characters"
 
 })
